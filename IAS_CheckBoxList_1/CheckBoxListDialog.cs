48930a552e3671c9e9b86fff2ba7e40c39097d23
Thomas Remmery <thomas.remmery@skyline.be>
2024-05-07 16:08:27 +0200

    Added demo showcasing a basic CheckBoxList.

---
+++ IAS_CheckBoxList_1/CheckBoxListDialog.cs
@@ -0,0 +1,47 @@
+﻿namespace IAS_CheckBoxList_1
+{
+    using Skyline.DataMiner.Automation;
+    using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+    using System;
+    using System.Collections.Generic;
+
+    public class CheckBoxListDialog : Dialog
+    {
+        private readonly CheckBoxList checkBoxList;
+        private readonly TextBox checkedOptionsTextBox;
+        private readonly Button exitButton;
+
+        private readonly IEnumerable<string> options = new string[]
+        {
+            "Yes",
+            "An Option",
+            "Another One",
+            "Option X",
+            "Something something Dark Side"
+        };
+
+        public CheckBoxListDialog(IEngine engine) : base(engine)
+        {
+            Title = "Select Option(s)";
+
+            // Set up checkboxlist
+            checkBoxList = new CheckBoxList(options) { IsSorted = true };
+            checkBoxList.Changed += (s, e) => checkedOptionsTextBox.Text = String.Join(Environment.NewLine, checkBoxList.Checked);
+
+            // Set up textbox
+            checkedOptionsTextBox = new TextBox { IsMultiline = true, Height = 150 };
+
+            // Set up exit button
+            exitButton = new Button("Exit");
+            exitButton.Pressed += (s, e) => OnExitButtonPressed?.Invoke(this, EventArgs.Empty);
+
+            // Generate Ui
+            AddWidget(new Label("Select Option(s)"), 0, 0, verticalAlignment: VerticalAlignment.Top);
+            AddWidget(checkBoxList, 0, 1);
+            AddWidget(checkedOptionsTextBox, 1, 0, 1, 2);
+            AddWidget(exitButton, 2, 0, 1, 2);
+        }
+
+        public event EventHandler OnExitButtonPressed;
+    }
+}
---
+++ IAS_CheckBoxList_1/IAS_CheckBoxList_1.cs
@@ -0,0 +1,118 @@
+/*
+****************************************************************************
+*  Copyright (c) 2024,  Skyline Communications NV  All Rights Reserved.    *
+****************************************************************************
+
+By using this script, you expressly agree with the usage terms and
+conditions set out below.
+This script and all related materials are protected by copyrights and
+other intellectual property rights that exclusively belong
+to Skyline Communications.
+
+A user license granted for this script is strictly for personal use only.
+This script may not be used in any way by anyone without the prior
+written consent of Skyline Communications. Any sublicensing of this
+script is forbidden.
+
+Any modifications to this script by the user are only allowed for
+personal use and within the intended purpose of the script,
+and will remain the sole responsibility of the user.
+Skyline Communications will not be responsible for any damages or
+malfunctions whatsoever of the script resulting from a modification
+or adaptation by the user.
+
+The content of this script is confidential information.
+The user hereby agrees to keep this confidential information strictly
+secret and confidential and not to disclose or reveal it, in whole
+or in part, directly or indirectly to any person, entity, organization
+or administration without the prior written consent of
+Skyline Communications.
+
+Any inquiries can be addressed to:
+
+	Skyline Communications NV
+	Ambachtenstraat 33
+	B-8870 Izegem
+	Belgium
+	Tel.	: +32 51 31 35 69
+	Fax.	: +32 51 31 01 29
+	E-mail	: info@skyline.be
+	Web		: www.skyline.be
+	Contact	: Ben Vandenberghe
+
+****************************************************************************
+Revision History:
+
+DATE		VERSION		AUTHOR			COMMENTS
+
+dd/mm/2024	1.0.0.1		XXX, Skyline	Initial version
+****************************************************************************
+*/
+
+namespace IAS_CheckBoxList_1
+{
+    using System;
+    using Skyline.DataMiner.Automation;
+    using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+
+    /// <summary>
+    /// Represents a DataMiner Automation script.
+    /// </summary>
+    public class Script
+    {
+        private InteractiveController app;
+
+        /// <summary>
+        /// The Script entry point.
+        /// IEngine.ShowUI();.
+        /// </summary>
+        /// <param name="engine">Link with SLAutomation process.</param>
+        public void Run(IEngine engine)
+        {
+            try
+            {
+                app = new InteractiveController(engine);
+
+                engine.SetFlag(RunTimeFlags.NoKeyCaching);
+                engine.Timeout = TimeSpan.FromHours(10);
+
+                RunSafe(engine);
+            }
+            catch (ScriptAbortException)
+            {
+                throw;
+            }
+            catch (ScriptForceAbortException)
+            {
+                throw;
+            }
+            catch (ScriptTimeoutException)
+            {
+                throw;
+            }
+            catch (InteractiveUserDetachedException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                engine.Log("Run|Something went wrong: " + e);
+                ShowExceptionDialog(engine, e);
+            }
+        }
+
+        private void RunSafe(IEngine engine)
+        {
+            CheckBoxListDialog dialog = new CheckBoxListDialog(engine);
+            dialog.OnExitButtonPressed += (s, e) => engine.ExitSuccess("Exit button pressed");
+            app.Run(dialog);
+        }
+
+        private void ShowExceptionDialog(IEngine engine, Exception exception)
+        {
+            ExceptionDialog exceptionDialog = new ExceptionDialog(engine, exception);
+            exceptionDialog.OkButton.Pressed += (sender, args) => engine.ExitFail("Something went wrong.");
+            if (app.IsRunning) app.ShowDialog(exceptionDialog); else app.Run(exceptionDialog);
+        }
+    }
+}
