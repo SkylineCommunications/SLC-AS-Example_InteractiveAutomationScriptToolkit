6f4cb2dea047bad5682ece39134b2a45747d9060
Rijad Comor <136588733+RijadCo@users.noreply.github.com>
2024-07-16 17:21:52 +0200

    new example script for HideUI method

---
+++ IAS_HideUI_1/HideUIPanel.cs
@@ -0,0 +1,44 @@
+﻿namespace IAS_HideUI_1
+{
+    using Skyline.DataMiner.Automation;
+    using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class HideUIPanel : Dialog
+    {
+        private readonly Label hideUiLabel = new Label("Hide UI:");
+
+        private readonly Label showUiAgainLabel = new Label("Show UI again after background action?");
+
+        public HideUIPanel(IEngine engine) : base(engine)
+        {
+            Title = "Hide UI";
+
+            Initialize();
+            GenerateUi();
+        }
+
+        public Button HideUiButton { get; set; }
+
+        public CheckBox HideUiCheckBox { get; set; }
+
+        private void Initialize()
+        {
+            HideUiButton = new Button("Hide");
+            HideUiCheckBox = new CheckBox { IsChecked = false };
+        }
+
+        private void GenerateUi()
+        {
+            Clear();
+
+            int row = -1;
+            AddWidget(hideUiLabel, ++row, 0);
+            AddWidget(HideUiButton, row, 1);
+            AddWidget(showUiAgainLabel, ++row, 0);
+            AddWidget(HideUiCheckBox, row, 1);
+        }
+    }
+}
